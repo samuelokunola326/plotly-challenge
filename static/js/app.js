cc27106d2d4969419ff2c9216722ae46012a8ef6
samuelokunola326 <samuelokunola@gmail.com>
2020-12-03 01:50:17 -0500

    adding demographic data

--- static/js/app.js
+++ static/js/app.js
@@ -10,15 +10,29 @@ var jsonData = "data/samples.json";
             console.log(names)
             dropdownData(names)
 
-            // names.forEach((names) => {
-            //     d3.select("#selDataset").append("option").text(names);
-            // })
+            getData(names[0])
+
+            
 
         });
             
     }
 
     initDD();
+
+    // function that gets the data based on ID/name
+function getData(id) {
+    d3.json(jsonData).then(function(sampledata) {
+        var metaResult = sampledata.metadata;
+        //console.log(metaResult);
+        var result = metaResult.filter(meta => meta.id.toString() === id)[0];
+        var demoInfo = d3.select("#sample-metadata");
+        demoInfo.html("");
+        Object.entries(result).forEach((key) => {   
+            demoInfo.append("h5").text(key[0] + ": " + key[1] + "\n");
+        });
+    });
+}
 
 
      // var sampleData = d3.json("samples.json").then(function(Data){
