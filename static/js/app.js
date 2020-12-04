84a50a8a0ff2378928eeedd66beb687e5a7c6579
samuelokunola326 <samuelokunola@gmail.com>
2020-12-04 03:42:30 -0500

    finalizing script + clean up + adding final comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,23 @@
 
-    
+//  accessing json data    
 var jsonData = "data/samples.json";
    
-    function initDD() {
+// funtion to load page 
+    function init() {
         d3.json(jsonData).then(function(sampledata) {
             console.log(sampledata);
             
+            // getting drop down values and passing into an array 
             var names = sampledata.names;
             console.log(names)
+            
+            // call back to funtion that creates drop down 
             dropdownData(names)
-
-            init(names[0])
-
+            
+            // call back to create default plot that shows
+            initPLT(names[0])
+            
+            // call back default values for demographic info 
             getData(names[0])
 
             
@@ -19,14 +25,16 @@ var jsonData = "data/samples.json";
         });
             
     }
+// call back to initial load of page 
+    init();
 
-    initDD();
 
-    // function that gets the data based on ID/name
+    // function that gets the data based on ID/name to filter for dem info
 function getData(id) {
     d3.json(jsonData).then(function(sampledata) {
         var metaResult = sampledata.metadata;
-        //console.log(metaResult);
+        
+        // filter func to filter by id use d3 to select div to place data received
         var result = metaResult.filter(meta => meta.id.toString() === id)[0];
         var demoInfo = d3.select("#sample-metadata");
         demoInfo.html("");
@@ -37,27 +45,22 @@ function getData(id) {
 }
 
 
-     // var sampleData = d3.json("samples.json").then(function(Data){
-    //     console.log(Data);
-    // });
-
-
-
+  
     // refernce dropdown  
 var selData = d3.select("#selDataset");
 
-// creating a function to loop through the table data
+// creating a function to call back later to create drop down data
 function dropdownData(x){
     
     x.forEach((x) => {
-        // using d3 to append a tr element for each Object in the Array of objects 
+        // using d3 to append a option element for each Object in the Array
         var opt = selData.append("option");
         
-        // then using the objects.entries to console log the objects 
+        // then using the objects.values to extract the values
         Object.values(x).forEach(([value]) => {
             // console.log(value);
             
-            //append the values to each row and input into cells
+            //append the values to each option and input into option value slot
             var val = opt.append("value");
             val.text(value); 
         });
@@ -65,39 +68,26 @@ function dropdownData(x){
 
 }
 
-// var t = ["a","b","c"]
-// dropdownData(t)
-
-// console.log(Object.getOwnPropertyNames)
-
-
-// turn into functions
-// var sortedSampleData = sampleData.sort((a, b) => b.sample_values - a.sample_values);
-
-// slicedData = sortedSampleData.slice(0, 10);
-
-// reversedData = slicedData.reverse();
-
-
-
-function init(id) {
+// creating funtion to replot charts after new selection is made from drop down 
+function initPLT(id) {
     d3.json(jsonData).then(function(sampledata) {
         console.log(sampledata.samples);
 
+        // using findIndex to return index position of selected id + displaying index
         var index = sampledata.samples.findIndex(sample => sample.id === id);
         console.log(`The index in data.samples array is: ${index}.`);
-
+        
+        // creating variable to pass to bar chart 
         var sampleValues = sampledata.samples[index]["sample_values"].slice(0,10).reverse()
         var sampleLabels = sampledata.samples[index]["otu_labels"].slice(0,10).reverse();
         var top_10_ids = (sampledata.samples[index]["otu_ids"].slice(0, 10)).reverse();
         var otu_id = top_10_ids.map(d => "OTU " + d);
-        // console.log(sampledata.samples[index].otu_ids)
+        
 
 
-
+// bar chart data + ploting bar chart
         var trace1 = {
-            // x: sampledata.samples[index]["sample_values"].slice(0,10).reverse(),
-            // y: sampledata.samples[index]["otu_ids"].slice(0,10).reverse(),
+           
             text: sampleLabels,
             x: sampleValues,
             y: otu_id,
@@ -120,11 +110,11 @@ function init(id) {
             }
         }
 
-        Plotly.newPlot("bar", data);
+        Plotly.newPlot("bar", data, layout);
 
         
 
-// plot for bubble chart 
+// bubble chart data + plotting bubble chart 
         var trace2 = {
             x: sampledata.samples[index]["otu_ids"],
             y: sampledata.samples[index]["sample_values"],
@@ -138,87 +128,23 @@ function init(id) {
 
         var data2 = [trace2];
 
-        var layout = {
+        var layout2 = {
             xaxis:{title: "OTU ID"},
             showlegend: false,
             height: 600,
             width: 1000
         };
 
-        Plotly.newPlot("bubble", data2, layout);
+        Plotly.newPlot("bubble", data2, layout2);
 
     });
         
 }
     
-    // init();
-
+    
+// function listening for when a new id number is selected from the drop down 
     function optionChanged(id) {
-        init(id);
+        initPLT(id);
         getData(id);
     };
-    //     // Call updatePlotly() when a change takes place to the DOM
-    // d3.selectAll("#bar").on("change", updatePlotly);
-
-
-    // function updatePlotly() {
-    //     // Use D3 to select the dropdown menu
-    //     var dropdownMenu = d3.select("#bar").node();
-    //     // Assign the value of the dropdown menu option to a variable
-    //     var dataset = dropdownMenu.property("value");
-      
-    //     // Initialize x and y arrays
-    //     var x = [];
-    //     var y = [];
-      
-    //     if (dataset === 'dataset1') {
-    //       x = [1, 2, 3, 4, 5];
-    //       y = [1, 2, 4, 8, 16];
-    //     }
-      
-    //     else if (dataset === 'dataset2') {
-    //       x = [10, 20, 30, 40, 50];
-    //       y = [1, 10, 100, 1000, 10000];
-    //     }
-      
-    //     // Note the extra brackets around 'x' and 'y'
-    //     Plotly.restyle("plot", "x", [x]);
-    //     Plotly.restyle("plot", "y", [y]);
-    //   }
-      
-    
-      
-
-    //   function getData() {
-
-    //     var dropdownMenu = d3.select("#selectDataset");
-
-    //     var dataset = dropdownMenu.property("value");
-
-
-    //     var data = [];
-
-    //     if () {
-
-    //     }
-    //     else if () {
-
-    //     }
-    //   }
-
-
    
-    // d3.json("../samples.json").then(function(samples) {
-
-//     console.log(samples);
-  
-    // log a list of names
-    // var names = tvData.map(data => data.name);
-    // console.log("names", names);
-  
-    // Cast each hours value in tvData as a number using the unary + operator
-    // tvData.forEach(function(data) {
-    //   data.hours = +data.hours;
-    //   console.log("Name:", data.name);
-    //   console.log("Hours:", data.hours);
-    // });
